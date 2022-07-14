9907b9d85673e45ac3ef2d17de499d34a8409539
jungai <juworaphol125@gmail.com>
2022-07-14 20:35:20 +0700

    feat: example for lib(types, enum)

--- apps/nest-service/src/app/app.service.ts
+++ apps/nest-service/src/app/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { TLoveList } from '@next-backend/types';
+import { Love } from '@next-backend/enums';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to nest-service!' };
+  getData() {
+    const list: TLoveList = ['naruk', 'iu', 'rose'];
+    return {
+      message: `Welcome to nest-service! ${list.join()}`,
+      love: Object.values(Love),
+    };
   }
 }
---
+++ libs/enums/src/lib/love.enum.ts
@@ -0,0 +1,4 @@
+export enum Love {
+  ZERO = 'naruk',
+  FIRST = 'IU',
+}
