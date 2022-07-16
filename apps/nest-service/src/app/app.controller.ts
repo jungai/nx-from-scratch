42f2fe3b743b0a2f54497b4e0794eff964d41de4
jungai <juworaphol125@gmail.com>
2022-07-16 16:56:33 +0700

    feat: add example prisma module

--- apps/nest-service/src/app/app.controller.ts
+++ apps/nest-service/src/app/app.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseInterceptors,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { IuInterceptor } from '@next-backend/nest/interceptors';
+import { PrismaService } from '@next-backend/nest/prisma';
 
 import { AppService } from './app.service';
 
 @UseInterceptors(IuInterceptor)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('prisma')
+  @HttpCode(HttpStatus.OK) // by default is OK but remind it!
+  async getSong() {
+    return await this.prisma.song.findMany();
+  }
+
+  @Post('prisma')
+  @HttpCode(HttpStatus.CREATED)
+  async create() {
+    return await this.prisma.song.create({ data: { name: 'iu iu iu' } });
   }
 }
--- apps/nest-service/src/app/app.module.ts
+++ apps/nest-service/src/app/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { NestPrismaModule } from '@next-backend/nest/prisma';
+import configs from '../configs';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configs],
+    }),
+    NestPrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
